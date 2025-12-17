6000114945d6270da3e234d0d7dc155ab060e994
Gurumuha <alfredjuma5@gmail.com>
2025-12-17 13:16:11 +0300

    Add comments to Flask app routes
    Added comments to clarify the purpose of each route and the main app.

--- tutorials/flask apy.py
+++ tutorials/flask apy.py
@@ -1,22 +1,21 @@
-import datetime
-
 from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__) #this is the main
 
 @app.route("/")
 def greet():
-    return "<h1>Get Started with Flask</h1>"
+    return "<h1>Get Started with Flask</h1>" #directs to homepage
 
 @app.route("/add")
 def brief():
-    return "<p1>Your test must run and work today. There's no longer tomorrow on this bs.</p1>"
+    return "<p1>Your test must run and work today. There's no longer tomorrow on this bs.</p1>" #just a paragraph on another page
 
 @app.route("/tables")
 def disp():
-    return render_template('table test.html')
+    return render_template('table test.html') #example of a table with details
 
 
 
 if __name__ == "__main__":
+
     app.run(debug=True)
