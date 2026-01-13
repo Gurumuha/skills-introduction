ba5c97dd3eb755c6b2e7175de5609aee39beccc1
Gurumuha <alfredjuma5@gmail.com>
2026-01-13 15:52:34 +0300

    Add schematic drawing for electrical components

--- tutorials/schema.py
+++ tutorials/schema.py
@@ -1,1 +1,13 @@
+import schemdraw
+import schemdraw.elements as elm
+p =schemdraw.Drawing()
+V =p.add(elm.SourceV().label('V', loc='left'))
+R =p.add(elm.Resistor().label('R').right())
+C=p.add(elm.Capacitor().label('C').right())
+L= p.add(elm.Inductor().label('L').right())
+
+p.add(elm.Ground().at(V.start))
+p.add(elm.Ground().at(L.end))
+
+p.draw()
 
