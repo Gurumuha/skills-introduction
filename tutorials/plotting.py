ff01526d24b446e9075d6d39bdeaed80ebe87234
Gurumuha <alfredjuma5@gmail.com>
2026-01-30 09:08:36 +0300

    pie chart

--- tutorials/plotting.py
+++ tutorials/plotting.py
@@ -1,1 +1,9 @@
 
+y = np.array([211000, 500000, 81000, 1400000, 450000, 210000, 7000])
+x = ["Salary & Benefits", "Startup Costs", "Monthly Bills", "Medical Supplies", "Technology", "Transportation", "Other"]
+
+font1 ={'family':'serif', 'color':'black', 'size':25}
+plt.title("Estimated Budget", fontdict= font1)
+plt.pie(y, labels= x, startangle=175)
+plt.show()
+
